a33b31650a079fc6c718b7a0fdb77cd27b3929ec
Latsuj <justal.kevin@gmail.com>
2020-09-08 00:37:03 +0800

    [FEATURE] Editing the xo config

--- .xo-config.js
+++ .xo-config.js
@@ -3,6 +3,7 @@ module.exports = {
     "rules" : {
         "camelcase": 0,
         "no-unused-vars": 0,
-        "unicorn/filename-case": 0
+        "unicorn/filename-case": 0,
+		"ava/no-import-test-files": 0
     }
 }
