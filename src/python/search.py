d92deb540cee8147bc685f1ab3d7b95776c6fa07
Latsuj <justal.kevin@gmail.com>
2020-09-10 12:50:43 +0800

    [FEATURE] Bypassing cloudflare with pupetter

--- src/python/search.py
+++
@@ -1,5 +0,0 @@
-import cloudscraper
-
-scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
-# Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
-print(scraper.get("https://www.kiss-anime.ws/").text)  # => "<!DOCTYPE html><html><head>..."
