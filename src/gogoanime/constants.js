7d1cac00479242decdc3168f583c0beb5ab7bf27
a s u n a <codeasaurousteam@gmail.com>
2021-05-18 13:24:07 -0500

    Fix gogoanime link.

--- src/gogoanime/constants.js
+++ src/gogoanime/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NAME: 'GOGOANIME',
-	URL_SEARCH: 'https://www3.gogoanime.pro/search?keyword=',
-	URL_BASE: 'https://www3.gogoanime.pro'
+	URL_SEARCH: 'https://www5.gogoanime.pro/search?keyword=',
+	URL_BASE: 'https://www5.gogoanime.pro'
 };
